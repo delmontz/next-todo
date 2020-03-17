9fc60103ca45e8ae834987c90190257ce91d7534
Takanuma Kouyu <k_takanuma@yumemi.co.jp>
2020-03-18 00:48:30 +0900

    fix: スタイル修正

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+`
+
 const Anchor = styled.a`
   font-size: 100px;
 `
 
 export default () => (
   <>
-    <Link href="/top">
-      <a>入り口</a>
-    </Link>
+    <Container>
+      <Link href="/top">
+        <a>入り口</a>
+      </Link>
+    </Container>
   </>
 )
--- pages/top.jsx
+++ pages/top.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   color: blue;
+  text-align: center;
 `
 
 export default () => <Title>こんにちは世界！！</Title>
